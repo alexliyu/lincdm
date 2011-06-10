7ee0a4b47b3f204b4f96f80177812c8da9bd427d
alexliyu <alexliyu2012@gmail.com>
2011-06-10 15:51:42 +0800

    tt

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
- pip install -r requirements.txt
+sudo apt-get install python-sqlite
+pip install -r requirements.txt
--- settings.py
+++ settings.py
@@ -105,7 +105,7 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.i18n',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
