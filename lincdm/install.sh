445bd562af8e22bc003204b2ce23e8aa1d0ca85d
alexliyu <alexliyu2012@gmail.com>
2011-07-05 01:51:24 +0800

    tttt

--- lincdm/install.sh
+++ lincdm/install.sh
@@ -3,4 +3,10 @@ sudo apt-get install python-gevent
 sudo apt-get install python-memcache
 sudo apt-get install memcached
 sudo apt-get install libjpeg62 libjpeg62-dev zlib1g-dev
+sudo apt-get install libjpeg8-dev
+sudo apt-get install libzip-dev
+sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib/
+sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/
+sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/
 pip install -r requirements.txt
+
